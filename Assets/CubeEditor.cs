e2f3d1f09251358b260774d5cb501715af5c1f99
Stangil <stangil44@gmail.com>
2019-03-12 16:04:39 -0400

    Added text mesh to cubes and applied code to label each cube with coordinates dynamically

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
+[SelectionBase]
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+       
+        //textMesh.text = (transform.position.x.ToString()+ "," + transform.position.z.ToString());
+    }
 
     void Update()
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        
     }
 }
