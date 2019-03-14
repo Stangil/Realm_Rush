5c1c85f08c9d3ec6e6fa1501f40c72f5309fcf22
Stangil <stangil44@gmail.com>
2019-03-14 15:00:37 -0400

    Started search algorithm.

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -6,16 +6,67 @@ using UnityEngine;
 public class Pathfinder : MonoBehaviour
 {
     [SerializeField] Waypoint startWaypoint, endWaypoint;
+
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    [SerializeField] bool isRunning = true;//TODO make private
+
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        Pathfind();
+       // ExploreNeighbors();
+    }
+
+    private void Pathfind()
+    {
+        //Equeue waypoints
+        queue.Enqueue(startWaypoint);
+        while(queue.Count > 0)
+        {
+            var searchCenter = queue.Dequeue();
+            print("Searchng from " + searchCenter); //TODO remove later
+            HaltIfEndFound(searchCenter);
+        }
+        print("Finished pathfinding?");
+    }
+
+    private void HaltIfEndFound(Waypoint searchCenter)
+    {
+        if (searchCenter == endWaypoint)
+        {
+            print("Stopped search, found endpoint");//TODO remove later
+            isRunning = false;
+        }
+    }
+    private void ExploreNeighbors()
+    {
+        
+       foreach(Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                Debug.LogWarning(explorationCoordinates +" waypoint does not exist");
+            }
+        }
     }
 
     private void ColorStartAndEnd()
     {
-        startWaypoint.SetTopColor(Color.blue);
+        startWaypoint.SetTopColor(Color.green);
         endWaypoint.SetTopColor(Color.red);
     }
 
